e0bf1bbb07bec24813de7f37b4a3fd17f1407839
anuththara <it21232640@my.sliit.lk>
2024-11-28 00:07:55 +0530

    Initial Commit

---
+++ FilmApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmApp
+//
+//  Created by Anuththara Divarathna on 2024-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmApp/FilmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAppApp.swift
+//  FilmApp
+//
+//  Created by Anuththara Divarathna on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct FilmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
